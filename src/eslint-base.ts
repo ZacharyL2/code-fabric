123bc9d6ec86a0d789c55903be0323af7306fdb9
islizeqiang <islizeqiang@gmail.com>
2022-01-08 23:52:34 +0800

    feat: update rules

--- src/eslint-base.ts
+++ src/eslint-base.ts
@@ -1,24 +1,7 @@
-import path from 'path';
-import fs from 'fs';
-import tsEslintConfig from './tsEslintConfig';
-
-const isTypescriptApp = fs.existsSync(path.resolve(process.cwd(), 'tsconfig.json'));
-
-const isReactApp = (() => {
-  const pkg = fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf-8');
-  return pkg.includes('"react"');
-})();
-
-const extraExtends = (() => {
-  if (isTypescriptApp) return ['plugin:@typescript-eslint/recommended'];
-  if (isReactApp) return ['plugin:react/recommended'];
-  return [];
-})();
-
 export default {
-  extends: ['eslint-config-airbnb-base', 'prettier'].concat(extraExtends),
-  parser: isTypescriptApp ? '@typescript-eslint/parser' : '@babel/eslint-parser',
-  plugins: ['eslint-comments', 'react', 'jest', 'unicorn', 'react-hooks'],
+  extends: ['eslint-config-airbnb-base', 'prettier'],
+  parser: '@babel/eslint-parser',
+  plugins: ['eslint-comments', 'jest', 'unicorn'],
   env: {
     browser: true,
     node: true,
@@ -28,24 +11,6 @@ export default {
     jasmine: true,
   },
   rules: {
-    'react/display-name': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/state-in-constructor': 0,
-    'react/static-property-placement': 0,
-    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/no-array-index-key': 'warn',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks deps of Hooks
-    'react/require-default-props': 0,
-    'react/jsx-fragments': 0,
-    'react/jsx-wrap-multilines': 0,
-    'react/prop-types': 0,
-    'react/forbid-prop-types': 0,
-    'react/sort-comp': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-one-expression-per-line': 0,
     'generator-star-spacing': 0,
     'function-paren-newline': 0,
     'import/no-unresolved': 0,
@@ -61,10 +26,6 @@ export default {
     'import/no-self-import': 0,
     'import/extensions': 0,
     'import/no-useless-path-segments': 0,
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/anchor-is-valid': 0,
     'sort-imports': 0,
     'class-methods-use-this': 0,
     'no-confusing-arrow': 0,
@@ -81,13 +42,12 @@ export default {
     'eslint-comments/no-unlimited-disable': 0,
     'no-param-reassign': 2,
     'space-before-function-paren': 0,
-    ...(isTypescriptApp ? tsEslintConfig : {}),
   },
   settings: {
     // support import modules from TypeScript files in JavaScript files
     'import/resolver': {
       node: {
-        extensions: isTypescriptApp ? ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] : ['.js', '.jsx'],
+        extensions: ['.js', '.jsx'],
       },
     },
     'import/parsers': {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { default as eslint } from './eslint';
 export { default as prettier } from './prettier';
 export { default as stylelint } from './stylelint';
